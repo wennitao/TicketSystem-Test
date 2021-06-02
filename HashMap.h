d30816a7bf77f7317223179e036c1232c9c81848
AegeanYan <1203616626@qq.com>
2021-06-02 21:11:06 +0800

    1

---
+++ HashMap.h
@@ -0,0 +1,13 @@
+//
+// Created by 罗皓天 on 2021/6/2.
+//
+
+#ifndef CODE_HASHMAP_H
+#define CODE_HASHMAP_H
+#include <functional>
+
+template<class Key , class Data , class Hash = std::hash<Key>>
+class HashMap{
+
+};
+#endif //CODE_HASHMAP_H
