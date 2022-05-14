4563d3e2e8ad64f0ce4107f324797855b16dae92
wennitao <wennitao@126.com>
2022-05-14 15:00:30 +0800

    update py

--- test.py
+++ test.py
@@ -6,15 +6,21 @@ folderName = input ()
 testcase_num = int(input ())
 
 os.system("./compile.sh")
+os.system("rm *.dat")
+os.system("rm log.txt")
 
 for i in range(1, testcase_num + 1):
     print("processing", i, "...")
-    # os.system("rm *.dat")
-    # os.system("rm log.txt")
-    os.system("./code < total_rollback/" + folderName + "/" + str(i) + ".in > force/" + str(i) + ".out")
+    os.system("./code < total_rollback/" + folderName + "/" + str(i) + ".in > force/" + folderName + "/" + str(i) + ".out")
 
-# for i in range(1, testcase_num + 1):
-#     print("evaluate", i, "...")
-#     ret = os.system("diff test/{}.out bin/naive_{}.out".format(i, i))
-#     if ret != 0:
-#         print("error in", i, "!")
+os.system("rm *.dat")
+os.system("rm log.txt")
+for i in range(1, testcase_num + 1):
+    print("processing", i, "...")
+    os.system("./rollback < total_rollback/" + folderName + "/" + str(i) + ".in > test/" + folderName + "/" + str(i) + ".out")
+
+for i in range(1, testcase_num + 1):
+    print("evaluate", i, "...")
+    ret = os.system("diff force/{}.out test/{}.out".format(i, i))
+    if ret != 0:
+        print("error in", i, "!")
