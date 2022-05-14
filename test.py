d74cd2a9c62caaa4beb210f026a6f0ce33cef813
wennitao <wennitao@126.com>
2022-05-14 15:09:05 +0800

    update py

--- test.py
+++ test.py
@@ -5,22 +5,27 @@ testcase_num = 11
 folderName = input ()
 testcase_num = int(input ())
 
-os.system("./compile.sh")
-os.system("rm *.dat")
-os.system("rm log.txt")
+def runForce():
+    os.system("./compile.sh")
+    os.system("rm *.dat")
+    os.system("rm log.txt")
 
-for i in range(1, testcase_num + 1):
-    print("processing", i, "...")
-    os.system("./code < total_rollback/" + folderName + "/" + str(i) + ".in > force/" + folderName + "/" + str(i) + ".out")
+    for i in range(1, testcase_num + 1):
+        print("processing", i, "...")
+        os.system("./code < total_rollback/" + folderName + "/" + str(i) + ".in > force/" + str(i) + ".out")
 
-os.system("rm *.dat")
-os.system("rm log.txt")
-for i in range(1, testcase_num + 1):
-    print("processing", i, "...")
-    os.system("./rollback < total_rollback/" + folderName + "/" + str(i) + ".in > test/" + folderName + "/" + str(i) + ".out")
+def runRollback():
+    os.system("rm *.dat")
+    os.system("rm log.txt")
+    for i in range(1, testcase_num + 1):
+        print("processing", i, "...")
+        os.system("./rollback < total_rollback/" + folderName + "/" + str(i) + ".in > test/" + str(i) + ".out")
 
-for i in range(1, testcase_num + 1):
-    print("evaluate", i, "...")
-    ret = os.system("diff force/{}.out test/{}.out".format(i, i))
-    if ret != 0:
-        print("error in", i, "!")
+def runEvaluate():
+    for i in range(1, testcase_num + 1):
+        print("evaluate", i, "...")
+        ret = os.system("diff force/{}.out test/{}.out".format(i, i))
+        if ret != 0:
+            print("error in", i, "!")
+
+runRollback()
